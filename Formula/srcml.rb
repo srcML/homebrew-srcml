aac79b27b69387a81aa2026aecb6d7966bef9e4b
Michael L. Collard <collard@uakron.edu>
2025-02-21 10:01:19 -0500

    Restrict to arm64 macos only

--- Formula/srcml.rb
+++ Formula/srcml.rb
@@ -1,9 +1,13 @@
 class Srcml < Formula
   desc "srcML: Conversion of source code to/from the srcML format, querying and manipulation of srcML"
   homepage "https://www.srcml.org"
-  url "http://66.228.46.28/srcml-1.0.0-macOS.tar.gz"
-  sha256 "153791424f7926b3cc25bf708973f8cb35e4eafb47df04f1f8eb7704be0c37d0"
+  url "http://66.228.46.28/srcml-1.0.0-macOS-arm64.tar.gz"
+  sha256 "f07849b1f0d1ff2e163b06e2339989dcb52a91e0b396599cada2489c2fb12656"
   version "1.0.0"
+
+  # Restrict installation to macOS ARM64 (Apple Silicon)
+  depends_on arch: :arm64
+  depends_on :macos
 
   def install
     prefix.install Dir["usr/local/*"]

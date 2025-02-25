00647978fcc62c0d8de9da227a867d24e4fcc5d5
Michael L. Collard <collard@uakron.edu>
2025-02-25 15:58:36 -0500

    Fix location of version

--- Formula/srcml.rb
+++ Formula/srcml.rb
@@ -2,9 +2,9 @@ class Srcml < Formula
   desc "Convert source code to and from the srcML format, querying and manipulation"
   homepage "https://www.srcml.org"
   url "http://66.228.46.28/srcml-1.0.0-macOS-arm64.tar.gz"
-  head "https://github.com/srcML/srcML.git", branch: "develop"
   version "1.0.0"
   sha256 "f07849b1f0d1ff2e163b06e2339989dcb52a91e0b396599cada2489c2fb12656"
+  head "https://github.com/srcML/srcML.git", branch: "develop"
 
   # Restrict installation to macOS ARM64 (Apple Silicon)
   depends_on arch: :arm64

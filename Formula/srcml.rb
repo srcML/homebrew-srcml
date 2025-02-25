4fea979ef61b4ef1921471532551f4f5f2cf430f
Michael L. Collard <collard@uakron.edu>
2025-02-25 15:50:33 -0500

    Add head attribute

--- Formula/srcml.rb
+++ Formula/srcml.rb
@@ -2,6 +2,7 @@ class Srcml < Formula
   desc "srcML: Conversion of source code to/from the srcML format, querying and manipulation of srcML"
   homepage "https://www.srcml.org"
   url "http://66.228.46.28/srcml-1.0.0-macOS-arm64.tar.gz"
+  head "https://github.com/srcML/srcML.git", branch: "develop"
   sha256 "f07849b1f0d1ff2e163b06e2339989dcb52a91e0b396599cada2489c2fb12656"
   version "1.0.0"
 

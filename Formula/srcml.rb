465d25011af0e06567af4e4e462c85474324a326
Michael L. Collard <collard@uakron.edu>
2025-03-08 15:22:44 -0500

    Fix shas

--- Formula/srcml.rb
+++ Formula/srcml.rb
@@ -6,10 +6,10 @@ class Srcml < Formula
 
   if Hardware::CPU.arm?
     url "http://66.228.46.28/srcml-1.0.0-macOS-arm64.tar.gz"
-    sha256 "7d60df428396bf8a52c81b2ba2f5d815c76c23b7"
+    sha256 "e9ac9838b91cf80055489fc6b8d61572d559b78e2dd6ef4062782d8729e9efdb"
   elsif Hardware::CPU.intel?
     url "http://66.228.46.28/srcml-1.0.0-macOS-x86_64.tar.gz"
-    sha256 "b02e73494f6ebeecb7ac3903a73d597a7fadc89e"
+    sha256 "b1df3d9c6f29b2ff0bcdf0a9d624057066236e0b63011f2e28caf44fb0d3f961"
   end
 
   depends_on :macos

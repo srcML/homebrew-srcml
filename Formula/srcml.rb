91ce9d3fd58c6caf098a414d5a4248730575245f
Michael L. Collard <collard@uakron.edu>
2025-03-08 15:20:31 -0500

    Update lib and setup x86_64 packages

--- Formula/srcml.rb
+++ Formula/srcml.rb
@@ -1,13 +1,17 @@
 class Srcml < Formula
   desc "Convert source code to and from the srcML format, querying and manipulation"
   homepage "https://www.srcml.org"
-  url "http://66.228.46.28/srcml-1.0.0-macOS-arm64.tar.gz"
   version "1.0.0"
-  sha256 "f07849b1f0d1ff2e163b06e2339989dcb52a91e0b396599cada2489c2fb12656"
   head "https://github.com/srcML/srcML.git", branch: "develop"
 
-  # Restrict installation to macOS ARM64 (Apple Silicon)
-  depends_on arch: :arm64
+  if Hardware::CPU.arm?
+    url "http://66.228.46.28/srcml-1.0.0-macOS-arm64.tar.gz"
+    sha256 "7d60df428396bf8a52c81b2ba2f5d815c76c23b7"
+  elsif Hardware::CPU.intel?
+    url "http://66.228.46.28/srcml-1.0.0-macOS-x86_64.tar.gz"
+    sha256 "b02e73494f6ebeecb7ac3903a73d597a7fadc89e"
+  end
+
   depends_on :macos
 
   def install

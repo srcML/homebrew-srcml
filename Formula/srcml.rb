f5a82546da65ed7e62809794fe406d729ffad008
Michael L. Collard <collard@uakron.edu>
2025-03-10 13:29:45 -0400

    Fix sha for x86

--- Formula/srcml.rb
+++ Formula/srcml.rb
@@ -9,7 +9,7 @@ class Srcml < Formula
     sha256 "49411fede70263e2cc5f2f4c35170b925eed4a82f79a16207a87214646c090ca"
   elsif Hardware::CPU.intel?
     url "http://66.228.46.28/srcml-1.0.0-macOS-x86_64.tar.gz"
-    sha256 "b1df3d9c6f29b2ff0bcdf0a9d624057066236e0b63011f2e28caf44fb0d3f961"
+    sha256 "b40f780cae20cc600c60f74d439d1a5088751fc3d2c832c93fc6e50d84bbc5e0"
   end
 
   depends_on :macos

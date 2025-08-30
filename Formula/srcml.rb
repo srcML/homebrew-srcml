09637d245565781188d7a3f5f28e116094af166a
Michael L. Collard <collard@uakron.edu>
2025-08-30 05:52:00 -0400

    Switch to special package name to monitor downloads

--- Formula/srcml.rb
+++ Formula/srcml.rb
@@ -3,11 +3,12 @@ class Srcml < Formula
   homepage "https://www.srcml.org"
   version "1.1.0"
 
+  # Use special package name to monitor installs
   if Hardware::CPU.arm?
-    url "https://github.com/srcML/srcML/releases/download/v1.1.0/srcml-1.1.0-macOS-arm64.tar.gz"
+    url "https://github.com/srcML/srcML/releases/download/v1.1.0/srcml-1.1.0-macOS-brew-arm64.tar.gz"
     sha256 "38e67facd9292b4a06fee0e7f6a4269a3218d0ccfabf1cb5c6ccadd57e36abc3"
   elsif Hardware::CPU.intel?
-    url "https://github.com/srcML/srcML/releases/download/v1.1.0/srcml-1.1.0-macOS-x86_64.tar.gz"
+    url "https://github.com/srcML/srcML/releases/download/v1.1.0/srcml-1.1.0-macOS-brew-x86_64.tar.gz"
     sha256 "7fa89298a60738edfe61179d0dcc57051c381ba53d917d5f5c69a9f8e964dca2"
   end
 
